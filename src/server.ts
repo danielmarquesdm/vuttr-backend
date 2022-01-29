25c4871ced79f349da1d2b93b97a2dd91e2ac4c6
Daniel Marques <danielvictorsm@gmail.com>
2022-01-29 11:00:45 -0300

    feat(Tool): Adição de cors

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,19 @@ import winston from "winston";
 import expressWinston from "express-winston";
 import "./database";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
 app.use(expressWinston.logger({
-    transports: [
-      new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.json(),
-      winston.format.prettyPrint()
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+    winston.format.prettyPrint()
     ),
     meta: false,
     msg: "HTTP  ",
@@ -23,6 +24,6 @@ app.use(expressWinston.logger({
     colorize: false,
     ignoreRoute: function (req, res) { return false; }
   }));
-
-app.use(routes);
-app.listen(3000, () => console.log("Server is running on port 3000"));
+  
+  app.use(routes);
+  app.listen(3000, () => console.log("Server is running on port 3000"));
